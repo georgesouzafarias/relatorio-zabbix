07e2ccfb7e940c9c2afc9004690a6e7e897da9f2
George Souza Farias <georgesouzafarias@gmail.com>
2015-11-17 14:16:22 -0400

    Adicionado os JAVADOC para a Classe GrupoHost e foram feita algumas alterações nos métodos de Adição de Host.

--- ZabbixProjeto/src/br/jus/tjrr/javaclasses/GrupoHost.java
+++ ZabbixProjeto/src/br/jus/tjrr/javaclasses/GrupoHost.java
@@ -2,28 +2,64 @@ package br.jus.tjrr.javaclasses;
 
 import java.util.ArrayList;
 
+/** Classe responsável por receber da API os grupos do Zabbix
+ * 
+ * @author George Farias
+ *
+ */
+
 public class GrupoHost {
 	
+		
 	private String nomeGrupo;
 	private ArrayList<Host> listHost;
 	private int idGrupo;
 	
 	
+	/**
+	 * Método para pegar o nome do Grupo
+	 * @return O nome do Grupo.
+	 */
 	public String getNomeGrupo() {
 		return nomeGrupo;
 	}
+	
+	/**
+	 * Método resposável por setar o nome do Grupo.
+	 * @param nomeGrupo String que representa o Nome do Grupo
+	 */
 	public void setNomeGrupo(String nomeGrupo) {
 		this.nomeGrupo = nomeGrupo;
 	}
+	
+	/**
+	 *  Método para listar os Hosts do Grupo
+	 * @return Um Array de Hots.
+	 */
 	public ArrayList<Host> getListHost() {
 		return listHost;
 	}
-	public void setListHost(ArrayList<Host> listHost) {
-		this.listHost = listHost;
+	
+	/**Método resposável por adicionar Hosts num Array.
+	 * 
+	 * @param host Recebe um Objeto do Tipo Host. 
+	 */
+	public void adicionarHost(Host host) {
+		this.listHost.add(host);
 	}
+	
+	/**
+	 * Método para pegar o ID do grupo
+	 * @return Retorna um Id do Grupo do tipo Inteiro. 
+	 */
 	public int getIdGrupo() {
 		return idGrupo;
 	}
+	
+	/**
+	 * Método para setar o ID do Grupo
+	 * @param idGrupo ID do grupo do tipo inteiro.
+	 */
 	public void setIdGrupo(int idGrupo) {
 		this.idGrupo = idGrupo;
 	}
